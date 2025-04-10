83155c68326658dfb9ea3f7569f346912c02e5e5
H Buter <112480089+h-buter@users.noreply.github.com>
2025-04-10 18:19:20 +0200

    Finished documenting

--- docs/pin_interrupts_8c.js
+++ docs/pin_interrupts_8c.js
@@ -2,5 +2,6 @@ var pin_interrupts_8c =
 [
     [ "port1ISR", "pin_interrupts_8c.html#af152e3894bb1d690bcbafb1559570436", null ],
     [ "port2ISR", "pin_interrupts_8c.html#a061e215759a676bd5115c41b89cabc6c", null ],
-    [ "lastDirection", "pin_interrupts_8c.html#ab59f73199aeb1a00b7b1e3d7f80625de", null ]
+    [ "lastDirection", "pin_interrupts_8c.html#ab59f73199aeb1a00b7b1e3d7f80625de", null ],
+    [ "toggle123", "pin_interrupts_8c.html#a989aa7a58248cf84b3f0bbf0a9f40332", null ]
 ];
--- docs/uart_8c.js
+++ docs/uart_8c.js
@@ -1,5 +1,6 @@
 var uart_8c =
 [
+    [ "UART_Bitstream", "uart_8c.html#a6ba1c3b797fa3288d5cf3a986716b811", null ],
     [ "UART_Init", "uart_8c.html#ad5cbed2a2222bb84e8b5c1caaa50634e", null ],
     [ "UART_SendChar", "uart_8c.html#a4a3cfbee206d916c5738bd0b14af5424", null ],
     [ "UART_SendInt", "uart_8c.html#a51f05743f75bc7debe328efd10fccdb8", null ],
